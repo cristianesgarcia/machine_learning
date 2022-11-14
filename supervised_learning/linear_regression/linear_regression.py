3efa2b09517e0316ca1ffa74d71dee397f59610c
cristiane <cristianesgcris@gmail.com>
2022-11-14 15:32:43 -0300

    Testing.

---
+++ supervised_learning/linear_regression/linear_regression.py
@@ -0,0 +1,22 @@
+import numpy as np
+from utils import messages
+
+class LinearRegression:
+    """Class to linear regression algorithms"""
+
+    print("Aqui")
+
+    # def __init__(self):
+    #     self.msg = messages.Messages()
+
+    # def normal_equation(self, x, y):
+    #     if x.size == 0 or y.size == 0:
+    #         raise Exception(self.msg.EMPTY_ARRAY)
+    #     elif x.size != y.size:
+    #         raise Exception(self.msg.DIFFERENT_SIZE)
+        
+    #     N = x.size
+    #     Xb = np.c_[np.ones((N,1)), x]
+    #     theta = np.linalg.inv(Xb.T.dot(Xb)).dot(Xb.T).dot(y)
+
+    #     return theta
